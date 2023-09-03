5ec4849231dc83607ff89e149975d1a101c7ead1
mserafin20 <mmserafin20@gmail.com>
2023-09-03 14:25:14 -0400

    server and client are connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Message from './components/Message';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ApolloProvider client={client}>
+      <Message />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +36,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      </ApolloProvider>
+
   )
 }
 
